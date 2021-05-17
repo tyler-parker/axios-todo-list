a7cb9f413d983f82b502fc7e12e7371d423e1da4
Tyler Parker <tparker42d2@gmail.com>
2021-05-16 21:50:15 -0600

    made progress on creating DOM elements and appending the API objects to the collection of items. Now to work on the POST request.

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-import axios from 'axios';
+// const axios = require("axios")
 
-
-axios.get('https://swapi.dev/api/people/1').then(response => {
-    console.log(response.data);
+axios.get('https://api.vschool.io/tyler-parker/todo')
+    .then(response => {
+      addListItem(response.data)
+      console.log(response.data);
   }).catch(error => {
-    console.log(error)
+      console.log(error)
   });
+
+function addListItem(items) {
+  items.forEach(data => {
+    const newItem = document.createElement('li')
+    const title = document.createElement('h5')
+    const description = document.createElement('p')
+    const img = document.createElement('img')
+    const breakLine = document.createElement('br')
+    const collection = document.getElementById("itemGroup")
+
+    title.textContent = data.title
+    description.textContent = data.description
+    img.src = data.imgUrl
+    img.setAttribute("class", "responsive-img")
+    newItem.setAttribute("class", "collection-item")
+
+    collection.appendChild(newItem)
+    newItem.append(title, description, img)
+    title.append(breakLine)
+    description.append(breakLine)
+    img.append(breakLine)
+  });
+}
