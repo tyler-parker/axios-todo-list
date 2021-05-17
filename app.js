d4a81ade0efa77270c2f8b65daf8a72a72b69b1a
Tyler Parker <tparker42d2@gmail.com>
2021-05-16 23:41:11 -0600

    i've added checkboxes to each item that displays but am having trouble with the event listener that toggles the completed status when checkbox is clicked

--- app.js
+++ app.js
@@ -14,19 +14,40 @@ function addListItem(items) {
     const title = document.createElement('h5')
     const description = document.createElement('p')
     const img = document.createElement('img')
-    const breakLine = document.createElement('br')
     const collection = document.getElementById("itemGroup")
+    const checkBox = document.createElement('input')
+
 
     title.textContent = data.title
     description.textContent = data.description
     img.src = data.imgUrl
     img.setAttribute("class", "responsive-img")
     newItem.setAttribute("class", "collection-item")
+    checkBox.setAttribute("type", "checkbox")
+    checkBox.setAttribute("class", "filled-in")
 
     collection.appendChild(newItem)
-    newItem.append(title, description, img)
-    title.append(breakLine)
-    description.append(breakLine)
-    img.append(breakLine)
+    newItem.append(title, checkBox, description, img)
+    title.append(checkBox)
+
+    if (data.completed === true) {
+      title.insertAdjacentHTML("afterbegin", "<label><input type='checkbox' class='filled-in' checked='checked'/><span></span></label>")
+    } else {
+      title.insertAdjacentHTML("afterbegin", "<label><input type='checkbox' class='filled-in' /><span></span></label>")
+    }
+
   });
 }
+
+const checkBox = document.getElementsByClassName("filled-in")
+checkBox.addEventListener("change", (e) => {
+  if (checkBox.checked) {
+  axios.put("https://api.vschool.io/tyler-parker/todo" + data._id, {completed: true})
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+  } else {
+  axios.put("https://api.vschool.io/tyler-parker/todo" + data._id, {completed: false})
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+}
+})
